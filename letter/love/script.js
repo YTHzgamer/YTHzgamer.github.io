197c05f3561eb66f48bbe69fa86896672ff86487
YTHzgamer <72077805+YTHzgamer@users.noreply.github.com>
2024-01-28 15:07:39 +0700

    Add files via upload

---
+++ letter/love/script.js
@@ -0,0 +1,35 @@
+function updateClock() {
+    var now = new Date();
+    var second = now.getSeconds();
+    var thoiDiemBatDau = new Date('2023-11-29T09:00:00');
+    var thoiDiemHienTai = new Date();
+    var soMiligiayDaTroiQua = thoiDiemHienTai - thoiDiemBatDau;
+    var soNamDaTroiQua = Math.floor(soMiligiayDaTroiQua / (1000 * 60 * 60 * 24 * 365.25));
+    var soThangDaTroiQua = Math.floor((soMiligiayDaTroiQua % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.4375));
+    var soNgayDaTroiQua = Math.floor((soMiligiayDaTroiQua % (1000 * 60 * 60 * 24 * 30.4375)) / (1000 * 60 * 60 * 24));
+    var soGioDaTroiQua = Math.floor((soMiligiayDaTroiQua % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var soPhutDaTroiQua = Math.floor((soMiligiayDaTroiQua % (1000 * 60 * 60)) / (1000 * 60));
+    var clockElement = document.getElementById("clock");
+    clockElement.innerHTML = `
+    <div class="flip-clock">${soNamDaTroiQua}</div>
+    <div class="time">Y</div>
+    <div class="flip-clock">${soThangDaTroiQua}</div>
+    <div class="time">M</div>
+    <div class="flip-clock">${soNgayDaTroiQua}</div>
+    <div class="time">d</div>
+    <div class="flip-clock">${soGioDaTroiQua}</div>
+    <div class="time">h</div>
+    <div class="flip-clock">${soPhutDaTroiQua}</div>
+    <div class="time">m</div>
+    <div class="flip-clock">${second}</div>
+    <div class="time">s</div>
+
+        `;
+
+        var flipClocks = document.getElementsByClassName("flip-clock");
+        for (var i = 0; i < flipClocks.length; i++) {
+            flipClocks[i].style.backgroundColor = 000000;
+        }
+    }
+
+    setInterval(updateClock, 1000);
